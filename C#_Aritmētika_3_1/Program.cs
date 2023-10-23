fc79d7acc1c5bff0cb3a3c0442f7a9ad97042696
Grinchs <marcisg2345@gmail.com>
2023-10-23 22:29:24 +0300

    Add project files.

---
+++ C#_Aritmētika_3_1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Ievadi skaitli x ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < 5; i++)
+{
+    x++;
+    Console.WriteLine("Inkramentēšanas rezultāts: " + x);
+}
+
+Console.WriteLine();
+
+Console.WriteLine("Ievadi otru skaitli");
+x = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < 5; i++)
+{
+    x--;
+    Console.WriteLine("Dekrementēšanas rezultāts: " + x);
+}
+
+Console.ReadLine();
+
